b7e40324ce68711bf01890998a996c9e74fb3d58
= <=>
2022-10-14 13:18:36 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            while (true)
+            {
+                Console.WriteLine("Please, enter the word, or input 'stop' to exit");
+                string palindrome = Console.ReadLine();
+                if (palindrome == "stop")
+                {
+                    break;
+                }
+                    
+                char[] array = palindrome.ToCharArray();
+             
+                Array.Reverse(array);
+                   
+                string p_reverse = new string(array);
+                  
+                if (palindrome.Equals(p_reverse))
+                    {
+                        Console.WriteLine("Yay! Your word is a palindrome!");
+                    }
+                  
+                else
+                    {
+                        Console.WriteLine("Nay. Your word is NOT a palindrome!");
+                    }
+                
+            }
+            
+        }
+    }
+}
